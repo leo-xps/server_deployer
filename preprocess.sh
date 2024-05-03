3313d822ffb0152f4730f06fde4ceae43ff8e333
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2024-05-03 14:09:54 +0800

    chore: preprocess-fix

--- preprocess.sh
+++ preprocess.sh
@@ -11,4 +11,7 @@ jq --arg GAME_SERVER "$GAME_SERVER" '.Server = $GAME_SERVER' /ROOT/BUILD/Server_
 
 echo "Running PM2 Command"
 
-pm2-runtime pm2.json
+# pm2-runtime pm2.json 
+
+# run pm2-runtime with the pm2.json file in daemon mode
+pm2-runtime pm2.json --json --silent
